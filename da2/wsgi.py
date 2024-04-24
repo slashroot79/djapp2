e947afe2dcc58acc917b58c7fc8baddd1cdc6867
Ragu Karuturi <ragukaruturi@microsoft.com>
2024-04-24 12:45:57 -0500

    c5

--- da2/wsgi.py
+++ da2/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 import django
 
+for k in sorted(os.environ.keys()):
+    v = os.environ[k]
+    print ('%-30s %s' % (k,v[:70]))
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'da2.settings')
