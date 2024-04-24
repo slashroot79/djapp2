4079772c97d12c39c5ee432c6174e8bd1e2b0a49
Ragu Karuturi <ragukaruturi@microsoft.com>
2024-04-24 15:28:26 -0500

    c7

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/antenv/lib/site-packages"
+export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/antenv/lib/python3.11/site-packages"
 GUNICORN_CMD_ARGS="--timeout 600 --chdir=/home/site/wwwroot" gunicorn da2.wsgi:application
