480e4aab656a4d10daecb62db59aa7616dd41ee5
Ragu Karuturi <slashroot79@gmail.com>
2024-05-02 12:11:28 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/antenv/lib/python3.11/site-packages"
-python manage.py collectstatic --no-input
 GUNICORN_CMD_ARGS="--timeout 600 --chdir=/home/site/wwwroot" gunicorn da2.wsgi:application
